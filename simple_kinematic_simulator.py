97db5a0b4144cc3ab2b4ed2e1fc7c43744afa463
Mårtensson, Laura Vikke <9077020219@hanyang.ac.kr>
2022-03-03 18:27:25 +0100

    updates

--- simple_kinematic_simulator.py
+++ simple_kinematic_simulator.py
@@ -1,3 +1,4 @@
+
 import shapely
 from shapely.geometry import LinearRing, LineString, Point, MultiLineString
 from numpy import sin, cos, pi, sqrt
@@ -5,127 +6,142 @@ from random import random
 import matplotlib.pyplot as plt
 import math
 
-def new_coordinates(point_one, point_two):
+# A simulation of a differential-drive robot with x sensors
 
-    '''
-    Based on "The intercept theorem", also known as "Thales' theorem"
-    https://en.wikipedia.org/wiki/Intercept_theorem
-    '''
+# CONSTANTS
 
-    dx = (point_two[0] - point_one[0])
-    dy = (point_two[1] - point_one[1])
-
-    x_a = point_one[0] - dx/0.5
-    y_b = point_one[1] - dy/0.5
-
-    return (round(x_a), round(y_b))
-
-# A prototype simulation of a differential-drive robot with one sensor
-
-# Constants
-###########
 R = 0.02  # radius of wheels in meters
 L = 0.10  # distance between wheels in meters
 
 W = 10.0  # width of arena
 H = 10.0  # height of arena
 
-robot_timestep = 0.1        # 1/robot_timestep equals update frequency of robot
-simulation_timestep = 0.1  # timestep in kinematics sim (probably don't touch..)
+robot_timestep = 0.1        # 1/robot_timestep (10) equals update frequency of robot
+simulation_timestep = 0.1  # timestep in kinematics simulation (probably don't touch..)
 
-# the world is a rectangular arena with width W and height H
+# the world is a quadratic arena with width W and height H
+# it contains walls of various sizes and directions to create a maze environment
 walls = [
         ((W/2,H/2),(-W/2,H/2),(-W/2,-H/2),(W/2,-H/2),(W/2,H/2)),
-        ((-0.75,-1),(-0.5,-0.25)),
-        ]
+        ((3,-5),(-3,-1)),
+        ((-1, 5), (-3, 2)), 
+        ((-1, -2.3), (1 , 0)),
+        ((5, 0), (3, -2)),
+        ((-2.0, 3.5), (1, 2))]
 world = MultiLineString(walls)
+goal = Point(4,4)
 print(world)
 
 
-# Variables 
-###########
 
-x = 0.0   # robot position in meters - x direction - positive to the right 
-y = 0.0   # robot position in meters - y direction - positive up
-q1 = 0.0   # robot heading with respect to x-axis in radians 
-q2 = 0.75
-q3 = -0.75
+
+# VARIABLES
+
+x = -4.0   # robot position in meters - x direction - positive to the right 
+y = 4.0   # robot position in meters - y direction - positive up
+q_mid = 0.0 
+q_mid_left = 0.5
+q_mid_right = -0.5  # robot heading with respect to x-axis in radians 
+q_left = 1
+q_right = -1
 
 left_wheel_velocity =  random()   # robot left wheel velocity in radians/s
 right_wheel_velocity =  random()  # robot right wheel velocity in radians/s
 
-# Kinematic model
-#################
+
+
+
+
+# KINEMATIC MODEL
 # updates robot position and heading based on velocity of wheels and the elapsed time
 # the equations are a forward kinematic model of a two-wheeled robot - don't worry just use it
 def simulationstep():
-    global x, y, q1, q2, q3
+    global x, y, q_mid, q_mid_left, q_mid_right, q_left, q_right
 
-    for step in range(int(robot_timestep/simulation_timestep)):    #step model time/timestep times
-        v_x = cos(q1)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2) 
-        v_y = sin(q1)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2)
-        omega1 = (R*right_wheel_velocity - R*left_wheel_velocity)/(2*L)  
-        omega2 = (R*right_wheel_velocity - R*left_wheel_velocity)/(2*L)    
-        omega3 = (R*right_wheel_velocity - R*left_wheel_velocity)/(2*L) 
+    for step in range(int(robot_timestep/simulation_timestep)):    #step model time/timestep times (0.1/0.1)
+        v_x = cos(q_mid)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2) 
+        v_y = sin(q_mid)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2)
+        omega = (R*right_wheel_velocity - R*left_wheel_velocity)/(2*L)  
         
 
         x += v_x * simulation_timestep
         y += v_y * simulation_timestep
-        q1 += omega1 * simulation_timestep
-        q2 += omega2 * simulation_timestep
-        q3 += omega3 * simulation_timestep
-        print()
+        q_mid += omega * simulation_timestep
+        q_mid_left += omega * simulation_timestep
+        q_mid_right += omega * simulation_timestep
+        q_left += omega * simulation_timestep
+        q_right += omega * simulation_timestep
 
-        #print(x) 
-
-# Simulation loop
-#################
-file = open("trajectory.dat", "w")
-
-path = []
-
-for cnt in range(5000):
-    #simple single-ray sensor
+# SIMULATION LOOP
+# SENERE PROJEKT: lave 'log' over koordinater så vi kan gemme og se/plotte en specific robot iteration's rute senere
+plot = True
+f = open("coordinates.csv", "w")
+for cnt in range(1000):
     robot = LineString([(x-0.20,y-0.20), (x+0.20,y-0.20), (x+0.20,y+0.20), (x-0.20,y+0.20),(x-0.20,y-0.20)])
-    #magnitude = math.sqrt(((x+cos(q))**2) + ((y+sin(q))**2))
-    #vector = (((x+cos(q))/magnitude)/4, ((y+sin(q))/magnitude)/4)
-    #mag = math.sqrt(((x+cos(q))/magnitude)**2 + ((y+sin(q))/magnitude)**2 )
-    #ray = LineString([(x, y), vector ])  # a line from robot to a point outside arena in direction of q
-    ray1 = LineString([(x, y), ((x+cos(q1)),(y+sin(q1)))])  # a line from robot to a point outside arena in direction of q
-    ray2 = LineString([(x, y), ((x+cos(q2)),(y+sin(q2)))])
-    ray3 = LineString([(x, y), ((x+cos(q3)),(y+sin(q3)))])
-    s = world.intersects(ray1)
-    #print((x,y), vector)
-    #print(mag)
-    print(ray1.length)
-    print(ray2.length)
-    print(ray3.length)
-    #print(ray.xy)
-    #print(ray.length)
+    ray_mid = LineString([(x, y), ((x+cos(q_mid)),(y+sin(q_mid)))])  # a line from robot to a point outside arena in direction of q
+    ray_mid_left = LineString([(x, y), ((x+cos(q_mid_left)),(y+sin(q_mid_left)))])  
+    ray_mid_right = LineString([(x, y), ((x+cos(q_mid_right)),(y+sin(q_mid_right)))])  
+    ray_left = LineString([(x, y), ((x+cos(q_left)),(y+sin(q_left)))])
+    ray_right = LineString([(x, y), ((x+cos(q_right)),(y+sin(q_right)))])
+    s_mid = world.intersects(ray_mid)
+    s_mid_left = world.intersects(ray_mid_left)
+    s_mid_right = world.intersects(ray_mid_right)
+    s_left = world.intersects(ray_left)
+    s_right = world.intersects(ray_right)
+    #print(ray1.length)
+    f.write(str(x) + ',' + str(y) + '\n')
 
-
-    #distance = sqrt((s.x-x)**2+(s.y-y)**2)                    # distance to wall
-    
-    #
-    # path.append(Point(x,y).xy)
-    if cnt%50==0:
-        for line in world:
-            plt.plot(*line.xy)
-        plt.plot(*robot.xy)
-        plt.plot(*ray1.xy)
-        plt.plot(*ray2.xy)
-        plt.plot(*ray3.xy)
-        plt.show()
-
-    
-    #simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
-    if (s == True):
-        left_wheel_velocity = -0.4
-        right_wheel_velocity = 0.4
-    else:                
+    # PLOT THE ROBOT
+    if plot == True:
         if cnt%100==0:
+            plt.figure(figsize =(5,5))
+            plt.xticks(range(-5,5))
+            plt.yticks(range(-5,5))
+            for line in world:
+                plt.plot(*line.xy, color='black')
+            plt.plot(*robot.xy, color='blue')
+            plt.plot(*ray_mid.xy, color='red', linestyle='dashed')
+            plt.plot(*ray_mid_left.xy, color='red', linestyle='dashed')
+            plt.plot(*ray_mid_right.xy, color='red', linestyle='dashed')
+            plt.plot(*ray_left.xy, color='red', linestyle='dashed')
+            plt.plot(*ray_right.xy, color='red', linestyle='dashed')
+            print(cnt)
+            plt.pause(0.1)
+        
+    #simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
+    if (s_left == True): 
+        if ((s_mid == False) and (s_mid_left == False)): # Hvis væg på venstre side drej til højre of følg væggen
+            # FORWARD
+            left_wheel_velocity = 0.5
+            right_wheel_velocity = 0.5
+            print("ligeud")
+        
+        # Hvis robot er ved at ramme venstre væg drej til højre
+        if s_mid_left == True:
+            left_wheel_velocity = 0.5
+            right_wheel_velocity = -0.5
+            print("højre")
+
+    elif (s_mid == True): # Hvis væg lige foran robot, drej til højre
+        # RIGHT
+        left_wheel_velocity = 0.5
+        right_wheel_velocity = -0.5
+        print("højre")
+    
+    elif (s_mid == s_mid_left == s_mid_right == s_left == s_right == False): # hvis ingen vægge, drej til venstre
+        left_wheel_velocity = 0.5
+        right_wheel_velocity = 0.65
+        print("alt False")
+
+    else:   
+        left_wheel_velocity = 0.5
+        right_wheel_velocity = 0.5
+        print("hej")
+        
+    
+        """if cnt%100==0:
             left_wheel_velocity = random()
-            right_wheel_velocity = random()
+            right_wheel_velocity = random()"""
     
     """VORES ROBOT: Skal følge venstre væg/sensor
     If distance_forward>0.5 AND distance_left < 0.5:
@@ -136,15 +152,15 @@ for cnt in range(5000):
 
     #check collision with arena walls 
     if (world.distance(Point(x,y))<L/2):
-       break
-        
-    #if cnt%50==0:
-     #   file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.2) + ", " + str(sin(q)*0.2) + "\n")
+        break
+    if ((goal.distance(Point(x,y))<L/2)):
+        print('WINNER')
+        break
+f.close()
+if plot == True:
+    plt.show()
 
-#print(path)
-#pathAll = LineString([path])
 
 
 
-file.close()
     
